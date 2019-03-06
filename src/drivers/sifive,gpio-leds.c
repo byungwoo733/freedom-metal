124c5daaa2062117f4b9b0cc48c9b2b26205de67
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-03-06 23:15:40 +0000

    Revert "Define a public API for GPIO output"

--- src/drivers/sifive,gpio-leds.c
+++ src/drivers/sifive,gpio-leds.c
@@ -2,7 +2,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
 #include <string.h>
-#include <metal/gpio.h>
 #include <metal/drivers/sifive,gpio-leds.h>
 
 int  __metal_driver_led_exist (const struct metal_led *led, char *label)
@@ -21,8 +20,8 @@ void __metal_driver_led_enable (const struct metal_led *led)
 
     if (_led->gpio != NULL) {
 	/* Configure LED as output */
-        metal_gpio_disable_input(_led->gpio, _led->pin);
-        metal_gpio_enable_output(_led->gpio, _led->pin);
+        _led->gpio->vtable->disable_input(_led->gpio, (0x1 << _led->pin));
+        _led->gpio->vtable->enable_output(_led->gpio, (0x1 << _led->pin));
     }
 }
 
@@ -31,7 +30,7 @@ void __metal_driver_led_on (const struct metal_led *led)
     struct __metal_driver_sifive_gpio_led *_led = (void *)(led);
 
     if (_led->gpio != NULL) {
-        metal_gpio_set_pin(_led->gpio, led_pin, 1);
+        _led->gpio->vtable->output_set(_led->gpio, (0x1 << _led->pin));
     }
 }
 
@@ -40,7 +39,7 @@ void __metal_driver_led_off (const struct metal_led *led)
     struct __metal_driver_sifive_gpio_led *_led = (void *)(led);
 
     if (_led->gpio != NULL) {
-        metal_gpio_set_pin(_led->gpio, led_pin, 0);
+        _led->gpio->vtable->output_clear(_led->gpio, (0x1 << _led->pin));
     }
 }
 
@@ -49,7 +48,7 @@ void __metal_driver_led_toggle (const struct metal_led *led)
     struct __metal_driver_sifive_gpio_led *_led = (void *)(led);
 
     if (_led->gpio != NULL) {
-        metal_gpio_toggle_pin(_led->gpio, led_pin);
+        _led->gpio->vtable->output_toggle(_led->gpio, (0x1 << _led->pin));
     }
 }
 
